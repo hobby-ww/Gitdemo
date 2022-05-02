b140bb8b13a296f0bec3fef0df79dd5ace684594
jnu77deng <jnu77deng@163.com>
2022-05-02 16:59:56 +0800

    implement: a button for "-1"
    实现计数"-1"的按键

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -9,18 +9,23 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int var=0;
-    Button bt;
-    TextView tx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bt=findViewById(R.id.Add);
-        tx=findViewById(R.id.textView);
+       Button bt=findViewById(R.id.Add);
+       Button btSub=findViewById(R.id.sub);
+       TextView tx=findViewById(R.id.textView);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 tx.setText(String.valueOf(var++));
+            }
+        });
+        btSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tx.setText(String.valueOf(var--));
             }
         });
     }
