880a7d205177de422570db2bd4720480070ba944
jnu77deng <jnu77deng@163.com>
2022-05-02 16:43:21 +0800

    implement: a button for "+1"
    实现计数"+1"的按键

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int var=0;
+    Button bt;
+    TextView tx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt=findViewById(R.id.Add);
+        tx=findViewById(R.id.textView);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tx.setText(String.valueOf(var++));
+            }
+        });
     }
 }
